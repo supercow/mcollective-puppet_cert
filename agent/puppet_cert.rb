6e330f50f150399ae03b264b6ff7dbea6b93339a
James Sweeny <james.sweeny@puppetlabs.com>
2013-07-03 12:34:56 -0400

    Add action to remove local SSL data

--- agent/puppet_cert.rb
+++ agent/puppet_cert.rb
@@ -3,13 +3,56 @@ module MCollective
     class Puppet_cert<RPC::Agent
 
       require 'openssl'
+      require 'puppet'
+      require 'puppet/face'
       def startup_hook
-        #TODO need to find a good way to discover these
-        @ssldir = '/etc/puppetlabs/puppet/ssl'
-        @certname = 'localhost'
+        unless ::Puppet.settings.app_defaults_initialized?
+          ::Puppet.settings.preferred_run_mode = :agent
+          ::Puppet.settings.initialize_global_settings([])
+          ::Puppet.settings.initialize_app_defaults(::Puppet::Settings.app_defaults_for_run_mode(::Puppet.run_mode))
+        end
+        @ssldir = ::Puppet[:ssldir]
+        @certname = ::Puppet[:certname]
+      end
+
+      def remove_ssl_object
+        retval = nil
+        begin
+          retval = yield
+        rescue Exception => e
+          retval = e.message
+        end
+
+        case retval
+        when true
+          "Removed successfully."
+        when false
+          "Already removed."
+        else
+          retval
+        end
+      end
+
+      action "clean_self" do
+        reply[:own_cert] = remove_ssl_object {
+          ::Puppet::Face[:certificate,'0.0.1'].destroy @certname, {:ca_location => 'local'}
+        }
+
+        reply[:key] = remove_ssl_object {
+          ::Puppet::Face[:key,'0.0.1'].destroy @certname
+        }
+
+        reply[:cer] = remove_ssl_object {
+          ::Puppet::Face[:certificate_request,'0.0.1'].destroy @certname
+        }
+
+        reply[:ca_cert] = remove_ssl_object {
+          ::Puppet::Face[:certificate,'0.0.1'].destroy 'ca', {:ca_location => 'local'}
+        }
       end
 
       action "list" do
+        # would be really f'in sweet if I could use faces here...
         raw = File.read("#{@ssldir}/certs/#{@certname}.pem")
         cert = OpenSSL::X509::Certificate.new raw
 
